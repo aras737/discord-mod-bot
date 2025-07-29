41aa21a834bb09a53e8f77884da79ce3485952ab
aras737 <erdemiraraseymen936@gmail.com>
2025-07-29 18:42:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,11 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
+  intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
   console.log(`✅ Bot aktif: ${client.user.tag}`);
-});
-
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return;
-
-  if (message.content === '!ping') {
-    message.reply('🏓 Pong!');
-  }
 });
 
 client.login(process.env.TOKEN);
