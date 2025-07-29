d9ef23a4abeccc7a168e271adfa9f0ffe03b1b21
aras737 <erdemiraraseymen936@gmail.com>
2025-07-29 20:35:47 +0300

    Update and rename commands.js to commands/ping.js

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Botun pingini gösterir.'),
+  async execute(interaction) {
+    await interaction.reply('🏓 Pong!');
+  }
+};
