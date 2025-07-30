5c8b5c928d80e00f0d4bdce5cc3d3d2018351e43
aras737 <erdemiraraseymen936@gmail.com>
2025-07-30 11:19:10 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Botun tepki süresini gösterir'),
+    .setDescription('Botun gecikmesini gösterir.'),
   async execute(interaction) {
-    await interaction.reply('🏓 Pong!');
-  },
+    await interaction.reply(`🏓 Gecikme: ${Date.now() - interaction.createdTimestamp}ms`);
+  }
 };
