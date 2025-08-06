b4108ef806b4f453687698fc628a5b8e9e356192
aras737 <erdemiraraseymen936@gmail.com>
2025-08-06 19:53:44 +0300

    Create bilet.js

---
+++ commands/bilet.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('panel')
+    .setDescription('Bilet sistemi panelini kurar.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle('🎫 Destek Talebi Oluştur')
+      .setDescription('Herhangi bir sorununuz varsa aşağıdaki butona tıklayarak destek bileti oluşturabilirsiniz.\n\n📌 Lütfen sadece gerçekten yardıma ihtiyacınız varsa bilet oluşturun.\n\n⛔ Kurallara uymayan kullanıcıların bileti kapatılacaktır.')
+      .setColor('#2b2d31');
+
+    const button = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId('bilet-olustur')
+        .setLabel('📩 Bilet Oluştur')
+        .setStyle(ButtonStyle.Primary)
+    );
+
+    await interaction.reply({ embeds: [embed], components: [button] });
+  },
+};
