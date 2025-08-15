e110cd93c43bdb4bdf6bc6bea0987b43ecc7a9ef
aras737 <erdemiraraseymen936@gmail.com>
2025-08-15 22:16:11 +0300

    Update bot-yonet.js

--- commands/bot-yonet.js
+++ commands/bot-yonet.js
@@ -8,7 +8,7 @@ module.exports = {
     
     async execute(interaction) {
         // Bu komutu sadece bot sahibi kullanabilir.
-        // 'BOT_SAHİBİ_ID' yerine kendi Discord kullanıcı kimliğini (ID) yaz.
+        // '1389930042200559706' yerine kendi Discord kullanıcı kimliğini (ID) yaz.
         const ownerId = 'BOT_SAHİBİ_ID'; 
         if (interaction.user.id !== ownerId) {
             return interaction.reply({ content: 'Bu komutu kullanma yetkiniz yok.', ephemeral: true });
