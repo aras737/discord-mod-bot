fd58c8cb78bec0ddc728d79e2ecbc7cb0efca19a
aras737 <erdemiraraseymen936@gmail.com>
2025-11-12 22:13:31 +0300

    Fix formatting of allowed users list

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const commands = [];
 // 🔒 Sadece bu iki kullanıcı komut kullanabilir
 const ALLOWED_USERS = [
   "752639955049644034", // Kullanıcı 1
-  "1389930042200559706" // Kullanıcı 2
+  "1389930042200559706", // Kullanıcı 2
   "1336374889447293000" // Kullanıcı 3
 ];
 
