fc2322a346db81a6c8176b73746de3ef47e8ec20
aras737 <erdemiraraseymen936@gmail.com>
2025-11-12 11:24:01 +0300

    Fix command name typo from 'banlist' to 'banlis'

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('disc
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('banlist')
+        .setName('banlis')
         .setDescription('Sunucudaki yasaklı kullanıcıların listesini gösterir.')
         .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
 
